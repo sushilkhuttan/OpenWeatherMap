b49d39f13fd1bdf66ab6bdb70b28f13f4e7a595f
sushil <sushilkhuttan@gmail.com>
2023-06-11 04:18:27 +0100

    more assertions added

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # OpenWeatherMap
+Hi,
+Following are the details for my API automation plan, approach and scope.
+Tech: Java, Rest Assured, Testng, Maven
+Automation approach:I have automated 2 apis.
+Scope:
+Automate Current weather data API and verify the responses by using asserts. 
+Approach:
+1. I obtained the Longitude(lon) and Latitude(lat) values by automating Geocoding API and storing from the response.
+2. I used testng dataprovider to get lon and lat data for 2 cities.
+3. I used the obtained data from Geocoding API's response to feed into Current weather data API's url.
+4. I also sent the optional query parameters to test the api fully.
+5. Using an object for JsonPath, i extracted the data from response and using asserts i validated the response.
+
+Inscope tests:
+1. The response mandatory fields
+2. Testing logic. Example - 1. Max temp cannot be less than min temp. 2. Response is for the requested city
+3. Boundary level testing. Example - visibility
+4. Negative cases. 
+Out of Scope:
+Geocoding API testing 
--- src/OpenWeatherAPI.java
+++ src/OpenWeatherAPI.java
@@ -13,9 +13,9 @@ import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.*;
 
 public class OpenWeatherAPI {
-
+	String appid = "8f77bcfb6e181d97c6380d1ea53d9ff1";
 	RequestSpecification request = new RequestSpecBuilder().setBaseUri("http://api.openweathermap.org")
-			.addQueryParam("appid", "8f77bcfb6e181d97c6380d1ea53d9ff1").build();
+			.addQueryParam("appid", appid).build();
 
 	String lat;
 	String lon;
@@ -51,24 +51,26 @@ public class OpenWeatherAPI {
 				Double MaxTemp = jwp.getDouble("main.temp_max");
 				int timezone = jwp.getInt("timezone");
 				String id = jwp.getString("weather.id");
-				
+				int visibility = jwp.getInt("visibility");
 
 				
-		Assert.assertEquals(cityName, cityNameResponse); //Verify the city name is correct
+		Assert.assertTrue(cityNameResponse.contains(cityName)); //Verify the city name is correct
 		Assert.assertTrue(MaxTemp >= minTemp); //Verify the Max temperature is higher than min Temp
 		if(cityName == "London") {
 			Assert.assertEquals(timezone, 3600); //Verify timezone is constant 3600
 			Assert.assertNotNull(id);
 		}else if(cityName == "Chicago") {
 			Assert.assertEquals(timezone, -18000); //Verify timezone is constant -18000
+		}else if(cityName == "Madrid") {
+			Assert.assertEquals(timezone, 7200); //Verify timezone is constant 7200
 		}
-		
+		Assert.assertTrue(visibility <= 10000); //Boundary level test. Visibility max is 10 kms
 		
 	}
 	
 	@DataProvider(name = "dataprovider")
 	public Object[] data() {
-		return new Object[] {"London","Chicago"};
+		return new Object[] {"London","Chicago","Madrid"};
 		//return new Object[][] {{"London,metric"},{"Chicago,imperial"}};
 	}
 	
